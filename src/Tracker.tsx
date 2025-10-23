22f943f6d9bff9c0b169562db89ec3c70dd44b7b
Harith335 <harthb806@gmail.com>
2025-10-23 22:57:18 +0300

    iniT

--- src/Tracker.tsx
+++ src/Tracker.tsx
@@ -5,7 +5,7 @@ const Tracker = ({ total }) => {
   return (
     <Card className="w-full max-w-sm">
       <div className="ml-4 grid gap-4">
-        <Label className="text-gray-400">Total Expenses</Label>
+        {/* <Label className="text-gray-400">Total Expenses</Label> */}
         <Label className="font-bold text-4xl">${total}</Label>
       </div>
     </Card>
